7ed06731fa9c4a03628d38946840bd96a9794b1a
marcoppenheimer <51744472+marcoppenheimer@users.noreply.github.com>
2023-02-22 18:51:44 +0000

    fix: infer username from sasl.jaas.config (#13)

--- snap/local/exporter-wrapper.bash
+++ snap/local/exporter-wrapper.bash
@@ -13,6 +13,7 @@ PROPERTIES=$(cat "${SNAP_COMMON}"/exporter.properties)
 BOOTSTRAP_SERVERS=$(echo "${PROPERTIES}" | grep bootstrap.servers | sed 's/bootstrap\.servers=//g')
 SECURITY_PROTOCOL=$(echo "${PROPERTIES}" | grep security.protocol | sed 's/security\.protocol=//g')
 SASL_JAAS_CONFIG=$(echo $PROPERTIES | grep sasl.jaas.config | sed 's/sasl\.jaas\.config=//g')
+USERNAME=$(echo $SASL_JAAS_CONFIG | grep -oP "username\=\"([a-zA-Z0-9]+)" | cut -d "\"" -f 2)
 PASSWORD=$(echo $SASL_JAAS_CONFIG | grep -oP "password\=\"([a-zA-Z0-9]+)" | cut -d "\"" -f 2)
 
 for i in $(echo $BOOTSTRAP_SERVERS | tr "," "\n")
@@ -30,7 +31,7 @@ fi
 
 args+=" --sasl.enabled"
 args+=" --sasl.mechansim=scram-sha512"
-args+=" --sasl.username=exporter"
+args+=" --sasl.username=${USERNAME}"
 args+=" --sasl.password=${PASSWORD}"
 
 "${SNAP}"/opt/kafka/kafka_exporter "${args[@]}"
